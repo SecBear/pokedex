209f2c908c9f2048f1dfdc55c99c212020587723
Bryce Thorpe <thorpebryce@gmail.com>
2025-07-22 09:59:48 -0500

    feat: close readline interface on exit

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -3,5 +3,6 @@ import { State } from "./state.js"
 export function commandExit(state: State): void {
   const test = state.commands.exit.name
   console.log("Closing the Pokedex... Goodbye!")
+  state.rl.close() // close the readline interface
   process.exit(0)
 }
