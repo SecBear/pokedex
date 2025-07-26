fa7bc4a8d7108231be96fb2182f479e0ddc1595a
Bryce Thorpe <thorpebryce@gmail.com>
2025-07-26 17:22:20 -0500

    feat: display location names only

--- src/command_map.ts
+++ src/command_map.ts
@@ -4,7 +4,9 @@ export async function commandMap(state: State): Promise<void> {
   console.log(`Running map command! with: ${state.nextLocationsURL}`)
   try {
     const locations = await state.pokeAPI.fetchLocations(state?.nextLocationsURL)
-    console.log(locations)
+    for (let location of locations.results) {
+      console.log(location.name)
+    }
     // update state, store empty string if we're at the end
     state.prevLocationsURL = locations.previous ? locations.previous : ""
     state.nextLocationsURL = locations.next ? locations.next : ""
--- src/command_mapb.ts
+++ src/command_mapb.ts
@@ -5,7 +5,9 @@ export async function commandMapb(state: State): Promise<void> {
   console.log(`Running map back command with ${state.prevLocationsURL}`)
   try {
     const locations = await state.pokeAPI.fetchLocations(state?.prevLocationsURL)
-    console.log(locations)
+    for (let location of locations.results) {
+      console.log(location.name)
+    }
     // update state, store empty string if we're at the end
     state.prevLocationsURL = locations.previous ? locations.previous : "" 
     state.nextLocationsURL = locations.next ? locations.next : ""
